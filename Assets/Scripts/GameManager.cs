00059a33b7619846037a2f756077d823555ba3c4
Singyu Jo <singyujo@Singyuui-MacBook-Pro.local>
2018-10-27 03:44:23 +0900

    Deck card!

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject Prefab;
+	public Transform MyHandPosition;
+
+	void Awake() {
+		DOTween.Init();
+	}
+	// Use this for initialization
+	void Start () {
+		var card = Object.Instantiate(Prefab);
+		card.transform.SetParent(this.transform);
+		card.transform.DOMove(MyHandPosition.position, 3).SetEase(Ease.InBounce);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
