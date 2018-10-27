81edcd2dab32b50864cbad4ee79b0dccf5086b99
Singyu Jo <singyujo@Singyuui-MacBook-Pro.local>
2018-10-27 23:53:42 +0900

    .

---
+++ Assets/Scripts/Awaker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Awaker : MonoBehaviour {
+	public GameObject Target;
+	void Awake() {
+		if (Target != null) {
+			Target.SetActive(true);
+		}
+	}
+}
