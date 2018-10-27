571ee6b64460c9057a6f4cfb01354d7a0f8e2eb3
Singyu Jo <singyujo@Singyuui-MacBook-Pro.local>
2018-10-27 18:46:02 +0900

    .

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,5 +1,4 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace DeepDark
 {
@@ -10,13 +9,13 @@ namespace DeepDark
 	}
 	public class Card
 	{
-		public int Id { get; private set; }
-		public bool IsNegative { get; private set; }
-		public CardType CardType { get; private set; }
-		public string Name { get; private set; }
-		public int Cost { get; private set; }
-		public string Description { get; private set; }
-		public int Power { get; private set; }
-		public int HP { get; private set; }
+		public int Id { get; set; }
+		public bool IsNegative { get; set; }
+		public CardType CardType { get; set; }
+		public string Name { get; set; }
+		public int Cost { get; set; }
+		public string Description { get; set; }
+		public int Power { get; set; }
+		public int HP { get; set; }
 	}
 }
--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -1,17 +1,94 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+
 namespace DeepDark
 {
 	public class CardManager
 	{
-		
-
+		static CardManager Instance;
+		Dictionary<int, Card> Cards = new Dictionary<int, Card>();
 		public CardManager()
 		{
+			var id = 0;
+			{
+				var card = new Card();
+				card.Id = ++id;
+				card.Name = "직장 선배";
+				card.Description = "";
+				card.Cost = 3;
+				card.Power = 2;
+				card.HP = 2;
+				card.IsNegative = true;
+				Cards.Add(card.Id, card);
+			}
+			{
+				var card = new Card();
+				card.Id = ++id;
+				card.Name = "꼰대 대리";
+				card.Description = "";
+				card.Cost = 3;
+				card.Power = 2;
+				card.HP = 2;
+				card.IsNegative = true;
+				Cards.Add(card.Id, card);
+			}
+			{
+				var card = new Card();
+				card.Id = ++id;
+				card.Name = "만년 과장";
+				card.Description = "";
+				card.Cost = 3;
+				card.Power = 2;
+				card.HP = 2;
+				card.IsNegative = true;
+				Cards.Add(card.Id, card);
+			}
 
+
+			{
+				var card = new Card();
+				card.Id = ++id;
+				card.Name = "여자 친구";
+				card.Description = "";
+				card.Cost = 3;
+				card.Power = 2;
+				card.HP = 2;
+				card.IsNegative = false;
+				Cards.Add(card.Id, card);
+			}
+			{
+				var card = new Card();
+				card.Id = ++id;
+				card.Name = "소꼽 친구";
+				card.Description = "";
+				card.Cost = 3;
+				card.Power = 2;
+				card.HP = 2;
+				card.IsNegative = false;
+				Cards.Add(card.Id, card);
+			}
+			{
+				var card = new Card();
+				card.Id = ++id;
+				card.Name = "어머니";
+				card.Description = "";
+				card.Cost = 3;
+				card.Power = 2;
+				card.HP = 2;
+				card.IsNegative = false;
+				Cards.Add(card.Id, card);
+			}
 		}
 
-		public Card getCard(int id)
+		static public Card GetCard(int id)
 		{
+			if (Instance == null)
+			{
+				Instance = new CardManager();
+			}
+			Card value;
+			if (Instance.Cards.TryGetValue(id, out value))
+				return value;
 			return null;
 		}
 	}
