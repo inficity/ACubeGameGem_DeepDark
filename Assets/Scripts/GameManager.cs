854b5de904666b03187a948aee093e0605df03ff
Singyu Jo <singyujo@Singyuui-MacBook-Pro.local>
2018-10-28 12:52:00 +0900

    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -181,6 +181,7 @@ public class GameManager : MonoBehaviour {
 				case TurnActionEvent.CardUsed:
 				{
 					if (msg.playerId == NetworkManager.Instance.clientId) return;
+					if ((cardId / 100) % 2 != 0) return;
 					var card = SpawnCard(msg.cardId);
 					card.transform.position = OpUseCardFrom.transform.position;
 					AddDirection(true, close => {
